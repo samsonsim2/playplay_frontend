6aaecb726a12a7e33622ce0e75fa33737752f30b
Samson Sim <samsonsim2@gmail.com>
2023-07-30 14:53:30 +0800

    update games

--- src/utils/Games.js
+++ src/utils/Games.js
@@ -13,7 +13,70 @@ const games = [
         "title":"Medusa",
         "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "tags":["Sensitive", "Looking"]
-    }
+    },
+    {
+        "title":"Medusa",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Sensitive", "Looking"]
+    },
+    {
+        "title":"Rock, Paper, Scissors",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Finger-play"]
+    },
+    {
+        "title":"Grandma’s Underpants",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Speaking", "Voice only"]
+    },
+    {
+        "title":"Eat Poop You Cat ",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Finger-play", "Arms"]
+    },
+    {
+        "title":"Momentum",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Arms", "Legs"]
+    },
+
+    {
+        "title":"Lorem Ipsum 1",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Sensitive", "Voice only"]
+    },
+    {
+        "title":"Lorem Ipsum 2",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Looking", "Voice only"]
+    },
+    {
+        "title":"Lorem Ipsum 3",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Arms", "Legs", "Sensitive"]
+    },
+    {
+        "title":"Lorem Ipsum 4",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Looking", "Arm","Sensitive"]
+    },
+
+    {
+        "title":"Lorem Ipsum 5",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Looking", "Voice only"]
+    },
+    {
+        "title":"Lorem Ipsum 6",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Legs", "Voice only"]
+    },
+    {
+        "title":"Lorem Ipsum 7",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar fermentum tincidunt. Etiam quis tellus tincidunt dui euismod convallis non ac elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "tags":["Looking","Legs", "Voice only"]
+    },
+
     
 ]
 
