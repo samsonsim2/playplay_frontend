a6ccd4ee2e51ecf78d5c19e3f469061b108c379b
hWeitian <huangweitian1@hotmail.com>
2023-07-31 16:25:45 +0800

    Add not found page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
 import games from "./utils/Games";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      {games.map((game) => {
+      {/* <ThemeProvider> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </ThemeProvider> */}
+      {/* {games.map((game) => {
         return (
           <div>
             <h1>{game.title}</h1>
@@ -12,7 +24,7 @@ function App() {
             <h1>{game.tags}</h1>
           </div>
         );
-      })}
+      })} */}
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <img src="assets/Pngs/Background-09.png" alt="main logo" />
+    </>
+  );
+};
+
+export default Home;
