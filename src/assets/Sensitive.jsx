52ba5e12fd0019e1daf33c834519d6558e20d6f6
hWeitian <huangweitian1@hotmail.com>
2023-08-27 13:24:06 +0800

    Update stroke for sensitive and finger

--- src/assets/Sensitive.jsx
+++ src/assets/Sensitive.jsx
@@ -8,7 +8,7 @@ const SensitiveImage = ({ selected, handleClick }) => {
       onClick={() => handleClick("sensitive")}
       style={{ maxHeight: "90%" }}
       className={
-        selected ? "parts-stroke stroke-1 deselected" : "parts-stroke stroke-1"
+        selected ? "parts-stroke stroke-4 deselected" : "parts-stroke stroke-4"
       }
     >
       <polyline
@@ -18,25 +18,5 @@ const SensitiveImage = ({ selected, handleClick }) => {
     </svg>
   );
 };
-// const SensitiveImage = ({ selected, handleClick }) => {
-//   return (
-//     <svg
-//       id="Layer_1"
-//       data-name="Layer 1"
-//       xmlns="http://www.w3.org/2000/svg"
-//       viewBox="0 0 28.69 60.43"
-//       onClick={() => handleClick("sensitive")}
-//       style={{ maxHeight: "90%" }}
-//       className={
-//         selected ? "parts-stroke stroke-2 deselected" : "parts-stroke stroke-2"
-//       }
-//     >
-//       <polyline
-//         onClick={() => handleClick("sensitive")}
-//         points="7.68 59.29 27.56 31.36 1.13 31.36 22.36 1.13"
-//       />
-//     </svg>
-//   );
-// };
 
 export default SensitiveImage;
